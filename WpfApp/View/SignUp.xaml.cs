2c498eab0a8ca889c0dd24320cec8664f984742a
조정현 <emforhs0913@naver.com>
2023-11-22 08:06:24 +0900

    1122

---
+++ WpfApp/View/SignUp.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp.View
+{
+    /// <summary>
+    /// Login2.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SignUp : Window
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void MaximizeWindow(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState =
+                   (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+                   ? WindowState.Maximized : WindowState.Normal;
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MinimizeWindow(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void Hyperlink_terms(object sender, RequestNavigateEventArgs e)
+        {
+            try
+            {
+                // 링크 클릭 시 기본 웹 브라우저로 열기
+                Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+                e.Handled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error opening link: " + ex.Message);
+            }
+        }
+
+        private void Hyperlink_personal_info(object sender, RequestNavigateEventArgs e)
+        {
+            try
+            {
+                // 링크 클릭 시 기본 웹 브라우저로 열기
+                Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+                e.Handled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error opening link: " + ex.Message);
+            }
+        }
+
+        private void Hyperlink_login(object sender, RequestNavigateEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApp/ViewModel/SignUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp.Core;
+
+namespace WpfApp.ViewModel
+{
+    public class SignUp : ObservableObject
+    {
+        //생성자
+        public SignUp() 
+        {
+            // 연도, 월, 일에 대한 컬렉션 초기화
+            Years = new ObservableCollection<string>(Enumerable.Range(1900, DateTime.Now.Year - 1899).Select(year => year.ToString()));
+            Months = new ObservableCollection<string>(Enumerable.Range(1, 12).Select(month => month.ToString()));
+            Days = new ObservableCollection<string>(Enumerable.Range(1, 31).Select(day => day.ToString()));
+        }
+
+        //Public변수
+        public ObservableCollection<string> Years { get; set; }
+        public ObservableCollection<string> Months { get; set; }
+        public ObservableCollection<string> Days { get; set; }
+    }
+}
