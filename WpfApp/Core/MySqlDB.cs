d24a88490017fa86586c64d5a803fc35fe920ac9
조정현 <emforhs0913@naver.com>
2023-12-05 21:32:13 +0900

    1205

---
+++ WpfApp/Core/MySqlDB.cs
@@ -0,0 +1,97 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Core
+{
+    internal class MySqlDB : IDisposable
+    {
+        //변수
+        private MySqlConnection? _connection;
+        private readonly string _connectionString;
+
+        //생성자
+        public MySqlDB(string pConnectionString) 
+        {
+            _connectionString = pConnectionString;
+            Connection();
+        }
+
+        private void Connection()
+        {
+            _connection = new MySqlConnection(_connectionString);
+
+            try
+            {
+                _connection.Open();
+            } catch (Exception ex) 
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void AddParameters(MySqlCommand cmd, SqlParameter[]? parameters)
+        {
+            if (parameters != null)
+            {
+                foreach (SqlParameter param in parameters)
+                {
+                    cmd.Parameters.AddWithValue(param.Name, param.Value);
+                }
+            }
+        }
+
+        public IDataReader GetDataReader(string query) 
+        {
+            return GetReader(query, null);
+        }
+
+        public IDataReader GetReader(string query, SqlParameter[]? parameters) 
+        {
+            using MySqlCommand cmd = new MySqlCommand(query, _connection);
+
+            AddParameters(cmd, parameters);
+            return cmd.ExecuteReader();
+        }
+
+        public DataTable GetTable(string query)
+        {
+            return GetTable(query, null);
+        }
+
+        public DataTable GetTable(string query, SqlParameter[]? parameters)
+        {
+            using MySqlCommand cmd = new MySqlCommand(query, _connection);
+            AddParameters(cmd, parameters);
+            using MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        public long Execute(string query)
+        {
+            return Execute(query, null);
+        }
+
+        public long Execute(string query, SqlParameter[]? parameters)
+        {
+            using MySqlCommand cmd = new MySqlCommand(query, _connection);
+
+            AddParameters(cmd, parameters);
+            cmd.ExecuteNonQuery();
+            return cmd.LastInsertedId;
+        }
+
+        public void Dispose()
+        {
+            _connection?.Close();
+            _connection?.Dispose();
+            _connection = null;
+        }
+    }
+}
---
+++ WpfApp/Core/SqlParameter.cs
@@ -0,0 +1,17 @@
+﻿namespace WpfApp.Core
+{
+    public class SqlParameter
+    {
+        private readonly string _name;
+        private readonly object _value; 
+
+        public SqlParameter(string parameterName, object value)
+        {
+            this._name = parameterName;
+            this._value = value;
+        }
+
+        public string Name => _name;
+        public object Value => _value;
+    }
+}
